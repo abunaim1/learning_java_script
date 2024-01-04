b7950c0b18c2fb5c398eca3cfc104032cd29efc4
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-04 11:56:13 +0600

    basics

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("Hello world!");
+
+var lastName = "NAIM";
+
+console.log(lastName);
+console.log(typeof(lastName));
+var number1 = 17.4
+var number2 = 20
+// parseInt = this is convert a number to int.
+// parseFloat = this is convert a number to float
+
+
+//if else statements
+if (number1 == 17.4){
+    console.log("this is a floating point number");
+}
+else{
+    console.log("this is a integer number");
+}
