641a73a498194dced98a7dbd0b2af68ace1bd863
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-04 19:40:31 +0600

    function 2

--- function.js
+++ function.js
@@ -5,3 +5,12 @@ var a = 10;
 var b = 20;
 var total = addTwoElements(a, b);
 console.log(total);
+
+function test(){
+    return "Hello";
+}
+function test2(){
+    return test()+ " World";
+}
+var result = test2();
+console.log(result);
