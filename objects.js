1c68d1a4fb9c54d33280d76e8072085219dcbb5d
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-04 12:33:18 +0600

    objects learn

---
+++ objects.js
@@ -0,0 +1,22 @@
+var person = {
+    name: 'John',
+    age: 21,
+};
+var personcopy = {
+    name: 'John',
+    age: 21,
+    person2:{
+        name:'rator',
+        age: 32
+    }
+};
+
+
+// console.log(person);
+// console.log(person.name);
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(Object.entries(person));
+
+// console.log(personcopy);
+// console.log(personcopy.person2.name);
